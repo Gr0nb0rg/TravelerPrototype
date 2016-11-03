13d032a2c401865137caec6c4e1166e040137b66
Oliver <super-olly@hotmail.com>
2016-11-03 16:58:30 +0100

    Bow and arrow
    :octocat: basic bow arrow :laughing: :gun:

---
+++ Traveler/Assets/Scripts/Arrow.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class Arrow : MonoBehaviour
+{
+    private float velocity;
+    private float dist;
+    private float removeTime = 0;
+    private Vector3 spawnPoint;
+    private bool collided = false;
+
+    public float Velocity
+    {
+        set { velocity = value; }
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        transform.position += velocity*transform.forward*Time.fixedDeltaTime*50;   
+        //CheckDistance();
+
+        if (!collided) return;
+
+        Debug.Log("bababoi");
+        removeTime += Time.fixedDeltaTime;
+        if (removeTime >= 10)
+            Destroy(this.gameObject);
+    }
+
+    void CheckDistance()
+    {
+        //Do this? maybe bad idea, a lot of lagg...
+        dist += velocity;
+        if (!(dist >= 5)) return;
+
+        GetComponent<RopeGenerator>().Spawn();
+        dist = 0;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        collided = true;
+        velocity = 0f;
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
---
+++ Traveler/Assets/Scripts/Bow.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bow : MonoBehaviour {
+    
+
+    private float pullForce = 0.01f;
+    private float maxForce = 1f;
+    private float arrowSpeed = 0.0f;
+    private float forceTime = 0.0f;
+    private float maxForceTime = 5000f;
+
+    private bool steadyAim = true;
+    private bool bowLoaded = false;
+
+    public GameObject arrow;
+	void Start () {
+	
+	}
+	
+	void Update () {
+	    CheckBow();
+	}
+
+    private void CheckBow()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (!bowLoaded)
+                bowLoaded = true;
+
+            if (pullForce < maxForce)
+            {
+                pullForce += 0.01f*Time.fixedDeltaTime;
+            }
+        }
+
+        if (!Input.GetMouseButtonUp(0) || !bowLoaded) return;
+
+        //Create arrow here - may need cleaning...
+        Debug.Log(pullForce);
+        var player = GameObject.FindGameObjectWithTag("Player");
+        var a = Instantiate(arrow);
+        a.transform.position = transform.position + transform.forward * 1.2f;
+        a.transform.forward = new Vector3(player.transform.forward.x, -Camera.main.transform.rotation.x, player.transform.forward.z);
+        a.GetComponent<Arrow>().Velocity = (pullForce * 10);
+        pullForce = 0.02f;
+    }
+}
